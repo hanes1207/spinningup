c8a0a1b82314d131a2ae2a288f91940322dc544a
hanes1207 <hanes1207@kaist.ac.kr>
2026-03-03 16:00:27 +0900

    Disable tensorflow and solve Exercise 1.1

--- spinup/exercises/pytorch/problem_set_1/exercise1_1.py
+++ spinup/exercises/pytorch/problem_set_1/exercise1_1.py
@@ -28,7 +28,20 @@ def gaussian_likelihood(x, mu, log_std):
     #   YOUR CODE HERE    #
     #                     #
     #######################
-    return torch.zeros(1)
+    # sigma = torch.exp(log_std)
+    # log_likelihood = -0.5 * (torch.sum((((x - mu)**2 / sigma**2) + 2 * log_std), dim=1) + x.shape[1] * torch.log(2*torch.pi))
+    # k = action dimension (여기서는 10)
+    k = x.shape[1]
+    
+    # 1. 분자 부분: ((x - mu) / exp(log_std))^2
+    # 2. 분모/상수 부분: 2 * log_std (로그 성질 이용)
+    # 3. 파이 상수 부분: k * log(2 * pi)
+    
+    # 이 모든 걸 더한 뒤 -0.5를 곱합니다.
+    # dim=1로 합치되, 결과가 [batch]가 되도록 keepdim은 쓰지 않습니다.
+    pre_sum = ((x - mu) / torch.exp(log_std))**2 + 2 * log_std
+    log_likelihood = -0.5 * (torch.sum(pre_sum, dim=1) + k * np.log(2 * np.pi))
+    return log_likelihood
 
 
 if __name__ == '__main__':
